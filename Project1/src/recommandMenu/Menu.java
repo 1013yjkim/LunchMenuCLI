78f85bdeba43c48530e52069f9ddd2a4327e7147
1013yjkim <1013yjkim@naver.com>
2023-04-05 23:47:18 +0900

    [Update/Add] Menu's child Food
    음식 이름 저장

--- Project1/src/recommandMenu/Menu.java
+++ Project1/src/recommandMenu/Menu.java
@@ -9,3 +9,34 @@ abstract class Menu{
 	abstract void setCate(String cate);
 	abstract String getCate();
 }
+/**
+ * 음식의 이름을 담는 Menu의 자식클래스입니다.
+ * @author kimyejoo
+ *
+ */
+class Food extends Menu{
+	String foodName;
+
+	
+	public Food(String cate, String foodName) {
+		this.cate = cate;
+		this.foodName = foodName;
+
+	}//생성자 
+	
+	public void setName(String foodName) {
+		this.foodName = foodName;
+	}
+	@Override
+	void setCate(String cate) {
+		this.cate = cate;
+	}
+	@Override
+	String getCate() {
+		return cate;
+	}
+	String getFoodName() {
+		return cate+" - "+foodName;
+	}
+	
+}
